3877f04442620c91f0512fc562103c88ea9d43ac
Henrykeeh <c.henrique.silvabarbosa@gmail.com>
2025-05-22 16:41:36 -0300

    Criada a lógica das páginas Login e Cadastro

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,4 +1,4 @@
-import { FacebookLogo, InstagramLogo, LinkedinLogo } from '@phosphor-icons/react'
+import {InstagramLogoIcon, LinkedinLogoIcon, GithubLogoIcon } from '@phosphor-icons/react'
 
 function Footer() {
 
@@ -13,9 +13,15 @@ function Footer() {
                     </p>
                     <p className='text-lg'>Acesse nossas redes sociais</p>
                     <div className='flex gap-2'>
-                        <LinkedinLogo size={48} weight='bold' />
-                        <InstagramLogo size={48} weight='bold' />
-                        <FacebookLogo size={48} weight='bold' />
+                        <a href="https://www.linkedin.com/in/carlos-henrique-da-silva-barbosa-no-linked-in/" target="_blank">
+                            <LinkedinLogoIcon size={48} weight='bold' />
+                        </a>
+                        <a href="https://www.instagram.com/c.henrykeh/" target="_blank">
+                            <InstagramLogoIcon size={48} weight='bold' />
+                        </a>
+                        <a href="https://github.com/Henrykeeh" target="_blank">
+                            <GithubLogoIcon size={48} weight='bold' />
+                        </a>
                     </div>
                 </div>
             </div>
